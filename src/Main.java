ad14f22e748aba64a97d0b9d1072dd15fb29b0e3
techarenz <57818506+techarenz@users.noreply.github.com>
2023-09-13 07:55:30 +0800

    DONE

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner scanner;
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Please enter your age:");
+        scanner = new Scanner(System.in);
+        age = scanner.nextInt();
+        System.out.println("Please enter your first name:");
+        scanner = new Scanner(System.in);
+        firstName = scanner.next();
+        System.out.println("Please enter your favourite food:");
+        scanner = new Scanner(System.in);
+        favouriteFood = scanner.next();
+        System.out.println("First Name: " + firstName + "\n" + "\n" + "Age: " + age + "\n" + "\n" + "Favourite Food: " + favouriteFood );
 
 
     }
